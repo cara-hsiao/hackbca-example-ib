72cd461bec533f26e2ab35cc01461d0ef692e7c1
cara-hsiao <carahsiao2016@gmail.com>
2021-12-16 23:31:25 -0500

    Createda Projects Section

---
+++ data/dummy_projects_v2.js
@@ -0,0 +1,37 @@
+module.exports =  [
+    {
+        "project_id": 1,
+        "project_name": "Something",
+        "project_type": "Something",
+        "project_lead": "Soemthing",
+        "project_date_ymd": "2021-12-16",
+        "project_date": "DEC 16 (THURS)",
+        "project_time ": "9:30 AM",
+        "project_taken": 30,
+        "project_team": 10,
+        "project_description": "oops."
+    }, 
+    {
+        "project_id": 2,
+         "project_name": "Something",
+        "project_type": "Something",
+        "project_lead": "Soemthing",
+        "project_date_ymd": "2021-12-16",
+        "project_date": "DEC 16 (THURS)",
+        "project_time ": "9:30 AM",
+        "project_taken": 30,
+        "project_team": 10,
+        "project_description": "oops."
+    },
+    {
+        "project_name": "Something",
+        "project_type": "Something",
+        "project_lead": "Soemthing",
+        "project_date_ymd": "2021-12-16",
+        "project_date": "DEC 16 (THURS)",
+        "project_time ": "9:30 AM",
+        "project_taken": 30,
+        "project_team": 10,
+        "project_description": "oops."
+    }
+]
